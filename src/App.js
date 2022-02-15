f8bf6a0aac8e5e2f7a42042095df5d1b6434471e
964Priyanka <94621133+964Priyanka@users.noreply.github.com>
2022-02-15 15:14:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter ,Router, Route,} from "react-router-dom";
+import Recipelist from "./Recipelist"
+import Form from "./components/Form"
+import Login from "./components/Login"
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <BrowserRouter>
+    <Router>
+       <Route path="/" component={Form} />
+        <Route path= "/Login" component={Login} />
+     </Router>
+     </BrowserRouter>
+      <h1>Recipe app</h1>
+      <Recipelist/>
+      </div>
+     
   );
 }
-
+ 
 export default App;
